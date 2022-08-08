52bc0a731cef4264b4e8922bc3155b6133b914c3
PauloGustavo.lata <paulo.gustavo@mngs.com.br>
2022-08-08 09:39:36 -0300

    initial commit

---
+++ src/components/Layout/Layout.tsx
@@ -0,0 +1,13 @@
+import { Container, Text } from './styles';
+
+import React from 'react';
+
+const Layout: React.FC = () => {
+  return (
+    <Container>
+      <Text>Welcome to sunset sacolas</Text>
+    </Container>
+  );
+}
+
+export default Layout;
---
+++ src/components/Layout/styles.ts
@@ -0,0 +1,10 @@
+import styled from 'styled-components';
+
+export const Container = styled.div`
+  background: var(--primary);
+`;
+
+export const Text = styled.p`
+  color: var(--white);
+  font-size: 15px;
+`;
